1ef8a01c57c66a921fa37b7b8c9bd7900cbdda00
Facundo Carrizo <facundocarrizo99@gmail.com>
2024-10-28 20:35:02 -0300

    Se agregan tests y metodos

--- TPO_FitnessCenter/src/Modelo/Objetivo/BajarPeso.java
+++ TPO_FitnessCenter/src/Modelo/Objetivo/BajarPeso.java
@@ -13,4 +13,12 @@ public class BajarPeso extends Objetivo{
     public BajarPeso(int minutosEntreMax, int minutosEntreMin, int nivelAerobicoMin, int nivelAerobicoMax) {
         super(minutosEntreMax, minutosEntreMin, nivelAerobicoMin, nivelAerobicoMax);
     }
+
+    public double calcularPesoIdeal(Socio socio){
+        double alturaEnMetros = socio.getAltura() / 100.0;
+        int edad = socio.getEdad();
+        return 22 * alturaEnMetros * alturaEnMetros - (0.1 * edad);
+    }
+
+    
 }
--- TPO_FitnessCenter/src/Modelo/Socio.java
+++ TPO_FitnessCenter/src/Modelo/Socio.java
@@ -4,10 +4,62 @@ import Modelo.Objetivo.Objetivo;
 import Modelo.Peso.Peso;
 import java.util.Date;
 
-public class Socio {
+public class Socio inherits Usuario {
     private Date fechaNacimiento;
     private char sexo;
     private Peso peso;
     private double altura;
     private Objetivo objetivo;
+
+    public Socio(Date fechaNacimiento, char sexo, Peso peso, double altura, Objetivo objetivo) {
+        this.fechaNacimiento = fechaNacimiento;
+        this.sexo = sexo;
+        this.peso = peso;
+        this.altura = altura;
+        this.objetivo = objetivo;
+    }
+
+    public void cambiarObjetivo(Objetivo objetivo) {
+        this.objetivo = objetivo;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public char getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+
+    public Peso getPeso() {
+        return peso;
+    }
+
+    public void setPeso(Peso peso) {
+        this.peso = peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public Objetivo getObjetivo() {
+        return objetivo;
+    }
+
+    public void setObjetivo(Objetivo objetivo) {
+        this.objetivo = objetivo;
+    }
 }
