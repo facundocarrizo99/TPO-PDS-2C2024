493efabff5cd1a9681242e4f122089f394a36434
Samuel <samg.martinez2@gmail.com>
2024-11-09 15:11:52 -0300

    :construction: En desarrollo ElegirObjetivo en socioControlador

--- TPO_FitnessCenter/src/Controladores/SocioControlador.java
+++ TPO_FitnessCenter/src/Controladores/SocioControlador.java
@@ -53,6 +53,10 @@ public class SocioControlador {
 		
 	}
 	
+	public void elegirObjetivo() {
+		
+	}
+	
 	private Socio toModel(SocioDTO socio) {
         SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
         
